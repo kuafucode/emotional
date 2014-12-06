66086ab240e75e78489fb9651840eb3227768e1f
Ryan Sun <ryansun81@gmail.com>
2014-12-06 08:34:29 +0000

    local db setting

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'emotional',
+			'username'  => 'root',
+			'password'  => 'kuafu',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
