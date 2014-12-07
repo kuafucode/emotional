d46bd525ca02cf47c133b3de0a5230df3e940021
Fatih <fatihc84@gmail.com>
2014-12-07 21:52:01 +0200

    language selector in profile page

--- app/views/profile.php
+++ app/views/profile.php
@@ -32,6 +32,8 @@
                 onPictureAsBase64:base64_tofield_and_image
             });
 
+            $("#languageselector").val('<?php echo $user->languages;?>')
+
             $.get("face",{},
                 function(data,status){
                     var obj = jQuery.parseJSON( data);
@@ -232,7 +234,72 @@
                 <li>User Name: <?php echo $user->username; ?></li>
                 <li>Email: <?php echo $user->email; ?></li>
                 <li>Password: *****</li>
+                <li>Language: <select id="languageselector" name="languageselector">
+                        <option>Select a Language</option>
+                        <option value='af'>Afrikaans</option>
+                        <option value='sq'>Albanian</option>
+                        <option value='ar'>Arabic</option>
+                        <option value='hy'>Armenian</option>
+                        <option value='az'>Azerbaijani</option>
+                        <option value='eu'>Basque</option>
+                        <option value='be'>Belarusian</option>
+                        <option value='bg'>Bulgarian</option>
+                        <option value='ca'>Catalan</option>
+                        <option value='zh-CN'>Chinese (Simplified)</option>
+                        <option value='zh-TW'>Chinese (Traditional)</option>
+                        <option value='hr'>Croatian</option>
+                        <option value='cs'>Czech</option>
+                        <option value='da'>Danish</option>
+                        <option value='nl'>Dutch</option>
+                        <option value='et'>Estonian</option>
+                        <option value='tl'>Filipino</option>
+                        <option value='fi'>Finnish</option>
+                        <option value='fr'>French</option>
+                        <option value='gl'>Galician</option>
+                        <option value='ka'>Georgian</option>
+                        <option value='de'>German</option>
+                        <option value='el'>Greek</option>
+                        <option value='ht'>Haitian Creole</option>
+                        <option value='iw'>Hebrew</option>
+                        <option value='hi'>Hindi</option>
+                        <option value='hu'>Hungarian</option>
+                        <option value='is'>Icelandic</option>
+                        <option value='id'>Indonesian</option>
+                        <option value='ga'>Irish</option>
+                        <option value='it'>Italian</option>
+                        <option value='ja'>Japanese</option>
+                        <option value='ko'>Korean</option>
+                        <option value='la'>Latin</option>
+                        <option value='lv'>Latvian</option>
+                        <option value='lt'>Lithuanian</option>
+                        <option value='mk'>Macedonian</option>
+                        <option value='ms'>Malay</option>
+                        <option value='mt'>Maltese</option>
+                        <option value='no'>Norwegian</option>
+                        <option value='fa'>Persian</option>
+                        <option value='pl'>Polish</option>
+                        <option value='pt'>Portuguese</option>
+                        <option value='ro'>Romanian</option>
+                        <option value='ru'>Russian</option>
+                        <option value='sr'>Serbian</option>
+                        <option value='sk'>Slovak</option>
+                        <option value='sl'>Slovenian</option>
+                        <option value='es'>Spanish</option>
+                        <option value='sw'>Swahili</option>
+                        <option value='sv'>Swedish</option>
+                        <option value='th'>Thai</option>
+                        <option value='tr'>Turkish</option>
+                        <option value='uk'>Ukrainian</option>
+                        <option value='ur'>Urdu</option>
+                        <option value='vi'>Vietnamese</option>
+                        <option value='cy'>Welsh</option>
+                        <option value='yi'>Yiddish</option>
+                    </select></li>
+
             </ul>
+            <div class="btn">
+                <input type="submit" value="Save"/>
+            </div>
         </div>
     </div>
 </div>
